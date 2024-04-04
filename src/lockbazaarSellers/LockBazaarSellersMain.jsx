8cade9474a0eadf515fced2b604c3eb9aebd3b7c
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-04-04 14:53:26 -0700

    Only show seller listings in grid

--- src/lockbazaarSellers/LockBazaarSellersMain.jsx
+++ src/lockbazaarSellers/LockBazaarSellersMain.jsx
@@ -1,18 +1,16 @@
 import React, {useContext} from 'react'
 import ListingsDataGrid from './ListingsDataGrid.jsx'
 import LoadingContextLB from '../lockbazaarContext/LoadingContextLB.jsx'
-import FilterContext from '../context/FilterContext.jsx'
 import SellerHowTo from './SellerHowTo.jsx'
 import DBContext from '../app/DBContext.jsx'
 
 function LockBazaarSellersMain() {
 
-    const {filters} = useContext(FilterContext)
     const {validListings} = useContext(LoadingContextLB)
     const {profile = {}} = useContext(DBContext)
 
-    const listings = filters.seller
-        ? validListings.filter(listing => listing.seller === filters.seller)
+    const listings = profile && !profile?.isLBMod
+        ? validListings.filter(listing => listing.sellerName === profile?.username)
         : validListings
 
     return (
