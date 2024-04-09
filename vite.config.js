3e1c7716033c1beded25d3178fffee9cfcaec387
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-04-09 13:05:24 -0700

    vite = localhost

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
 import {visualizer} from 'rollup-plugin-visualizer'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+        host: 'localhost',
         port: 3000
     },
     plugins: [react(), visualizer()],
