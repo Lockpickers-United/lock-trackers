f21592961def48fb380830057f8efe903a703060
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-04-16 12:24:31 -0700

    load data.json from local snapshot

--- src/data/dataUrls.js
+++ src/data/dataUrls.js
@@ -6,6 +6,6 @@ if (import.meta.env && import.meta.env.VITE_LOCAL_DATA === 'true') {
     console.info('Attention: App is using LOCAL DATA.')
 }
 
-export const locksData = `${url}/data.json`
+export const locksData = '/data.json'
 export const jsonBackup = 'https://data.locktrackers.com/firebase/backup.json'
 export const lockListings = '/listings.json'
