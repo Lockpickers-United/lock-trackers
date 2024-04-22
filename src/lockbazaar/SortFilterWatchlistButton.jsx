330dfddf6354321075fc80fdfea32244fe1044c3
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-04-21 17:33:23 -0700

    simplify sort/filter watchlist button

--- src/lockbazaar/SortFilterWatchlistButton.jsx
+++ src/lockbazaar/SortFilterWatchlistButton.jsx
@@ -1,8 +1,6 @@
 import IconButton from '@mui/material/IconButton'
-import React, {useCallback, useContext, useMemo, useState} from 'react'
+import React, {useCallback, useContext, useMemo} from 'react'
 import Tooltip from '@mui/material/Tooltip'
-import Popover from '@mui/material/Popover'
-import SignInButton from '../auth/SignInButton'
 import AuthContext from '../app/AuthContext'
 import DBContext from '../app/DBContext'
 import FavoriteIcon from '@mui/icons-material/Favorite'
@@ -12,9 +10,6 @@ function SortFilterWatchlistButton() {
     const {isLoggedIn} = useContext(AuthContext)
     const {profile} = useContext(DBContext)
     const {filters, addFilter, removeFilter} =useContext(FilterContext)
-    const [anchorEl, setAnchorEl] = useState(null)
-    const open = Boolean(anchorEl)
-    const handleClose = useCallback(() => setAnchorEl(null), [])
 
     const watchlistView = useMemo(() => {
         return filters.collection === 'Watchlist'
@@ -26,17 +21,12 @@ function SortFilterWatchlistButton() {
 
     const handleChange = useCallback((event) => {
         event.preventDefault()
-        setAnchorEl(event.currentTarget)
-
-        if (isLoggedIn) {
             if (!watchlistView) {
                 addFilter('collection', 'Watchlist', true)
             } else {
                 removeFilter('collection')
             }
-        }
-
-    }, [addFilter, isLoggedIn, removeFilter, watchlistView])
+    }, [addFilter, removeFilter, watchlistView])
 
     const tooltipText = isLoggedIn ? 'View Watchlist' : 'Sign in to create Watchlist'
 
@@ -45,31 +35,11 @@ function SortFilterWatchlistButton() {
         : <FavoriteIcon fontSize={'medium'} color={hasWatchlist ? 'error' : 'inherit'}/>
 
     return (
-        <React.Fragment>
             <Tooltip title={tooltipText} arrow disableFocusListener>
                 <IconButton onClick={handleChange} style={{height: 40, width: 40, marginTop: 4}}>
                     {watchlistIcon}
                 </IconButton>
             </Tooltip>
-            {(!isLoggedIn && open) &&
-            <Popover
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left'
-                }}
-            >
-                <div style={{padding: 20}}>
-                    You must be signed in<br/>
-                    to use a Watchlist.<br/>
-                    <br/>
-                    <SignInButton onClick={handleClose}/>
-                </div>
-            </Popover>
-            }
-        </React.Fragment>
     )
 }
 
