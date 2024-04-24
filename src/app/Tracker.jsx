d36a4ccc0dc6f82d18ea6991220d34c734c8aa61
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-04-24 06:26:21 -0700

    fix: add page param to querystring for error tracking

--- src/app/Tracker.jsx
+++ src/app/Tracker.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import querystring from 'query-string'
 
-function Tracker({feature, ...extraParams}) {
+function Tracker({feature}) {
     if (import.meta.env.DEV) return null
     const randomStuff = (Math.random()).toString(36).substring(2, 10)
     const file = files[feature] || 'lt.gif'
     const ref = document.referrer || 'none'
-    const query = querystring.stringify({trk: feature, r: randomStuff, w: screen.width, ref, ...extraParams})
+    const page = window.location.href.replace(/.*\/#\/(\w+)\?*.*/,'$1')
+    const query = querystring.stringify({trk: feature, r: randomStuff, w: screen.width, ref, page})
     const url = `https://data.lpulocks.com/i/${file}?${query}`
     return <img alt='LPU Locks' src={url} width={0} height={0}/>
 }
