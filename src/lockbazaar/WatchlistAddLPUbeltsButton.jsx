afe3e84409ef71525978f8e260912b26b085bbd6
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-05-08 11:29:12 -0700

    add link to import on watchlist page

--- src/lockbazaar/WatchlistAddLPUbeltsButton.jsx
+++ src/lockbazaar/WatchlistAddLPUbeltsButton.jsx
@@ -6,50 +6,64 @@ import AuthContext from '../app/AuthContext.jsx'
 import SignInButton from '../auth/SignInButton.jsx'
 import Button from '@mui/material/Button'
 import WatchlistAddLPUbelts from './WatchlistAddLPUbelts.jsx'
+import {useNavigate} from 'react-router-dom'
 
 const WatchlistAddLPUbeltsButton = () => {
+
+    const {isLoggedIn} = useContext(AuthContext)
+    const navigate = useNavigate()
 
     const [open, setOpen] = useState(window.location.hash.includes('add'))
     const toggleOpen = useCallback(() => {
         setOpen(!open)
     }, [open])
 
-    const {isLoggedIn} = useContext(AuthContext)
+    const handleClick = useCallback((event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        navigate('/import')
+    }, [navigate])
+
 
     return (
-            <Accordion expanded={open} onChange={toggleOpen}
-                       style={{
-                           maxWidth: 700,
-                           marginLeft: 'auto',
-                           marginRight: 'auto',
-                           marginBottom: 10,
-                           backgroundColor: 'transparent'
-                       }}>
-                <AccordionSummary>
-                    <div style={{fontSize: '.9rem', textAlign: 'center', width: '100%'}}>
-                        <Button variant='contained' size='small' style={{backgroundColor: '#609cce', textTransform: 'none'}}>
-                            Add LPUbelts Lock to Watchlist
-                        </Button>
+        <Accordion expanded={open} onChange={toggleOpen}
+                   style={{
+                       maxWidth: 700,
+                       marginLeft: 'auto',
+                       marginRight: 'auto',
+                       marginBottom: 10,
+                       backgroundColor: 'transparent'
+                   }}>
+            <AccordionSummary>
+                <div style={{fontSize: '.9rem', textAlign: 'center', width: '100%'}}>
+                    <Button variant='contained' size='small'
+                            style={{backgroundColor: '#609cce', textTransform: 'none', marginRight: 10}}>
+                        Add LPUbelts Lock to Watchlist
+                    </Button>
+                    <Button variant='contained' size='small' onClick={handleClick}
+                            style={{backgroundColor: '#609cce', textTransform: 'none', marginLeft: 10}}>
+                        New! Import Your LPUbelts Wishlist
+                    </Button>
+                </div>
+            </AccordionSummary>
+            <AccordionDetails>
+                {!isLoggedIn &&
+                    <div style={{textAlign: 'center', alignItems: 'center'}}>
+                        Please log in to use a Watchlist<br/>
+                        <div style={{
+                            width: 210,
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginTop: 20,
+                            textAlign: 'center'
+                        }}><SignInButton/></div>
                     </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    {!isLoggedIn &&
-                            <div style={{textAlign: 'center', alignItems: 'center'}}>
-                                Please log in to use a Watchlist<br/>
-                                <div style={{
-                                    width: 210,
-                                    marginLeft: 'auto',
-                                    marginRight: 'auto',
-                                    marginTop: 20,
-                                    textAlign: 'center'
-                                }}><SignInButton/></div>
-                            </div>
-                    }
-                    {isLoggedIn &&
-                        <WatchlistAddLPUbelts/>
-                    }
-                </AccordionDetails>
-            </Accordion>
+                }
+                {isLoggedIn &&
+                    <WatchlistAddLPUbelts/>
+                }
+            </AccordionDetails>
+        </Accordion>
     )
 }
 
