09e51a3b46927079623b221d7836a15190866758
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-05-14 12:50:48 -0700

    useMemo to add profile to systemMessage dependencies

--- src/lockbazaarContext/MessageProviderLB.jsx
+++ src/lockbazaarContext/MessageProviderLB.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useContext, useMemo, useState} from 'react'
+import React, {useCallback, useContext, useMemo} from 'react'
 import MessageContext from '../app/MessageContext.jsx'
 import DBContext from '../app/DBContext.jsx'
 import DataContext from '../app/DataContext.jsx'
@@ -14,8 +14,6 @@ export function MessageProvider({children}) {
     const {getEntryFromId} = useContext(DataContext)
     const {watchlistIds} = useContext(WatchlistContextLB)
 
-    const [displayMessage, setDisplayMessage] = useState(false)
-    const [systemMessage, setSystemMessage] = useState(null)
 
     const handleDismiss = useCallback(() => {
         updateLastMessageDate({lastNewEntryDismiss: dayjs().format()})
@@ -23,27 +21,25 @@ export function MessageProvider({children}) {
 
 
     // New Listings Message
-    if (profile && filters?.collection !== 'Watchlist') {
-        const startDate = dayjs('2024-05-12T00:00:00.000Z')
-        const lastMessageDate = profile?.lastNewEntryDismiss ? dayjs(profile?.lastNewEntryDismiss) : startDate
-        const newListingIds = watchlistIds.filter(id => {
-            const newListingDate = getEntryFromId(id)?.newListingsDate ? dayjs(getEntryFromId(id).newListingsDate) : null
-            return (newListingDate && newListingDate.isAfter(lastMessageDate))
-        })
-        if (newListingIds?.length > 0 && !systemMessage) {
-            setSystemMessage(<NewListingsMessage entryCount={newListingIds?.length} handleDismiss={handleDismiss}/>)
-        }
-    }
 
+    const systemMessage = useMemo(() => {
+        if (profile && filters?.collection !== 'Watchlist') {
+            const startDate = dayjs('2024-05-14T00:00:00.000Z')
+            const lastMessageDate = profile?.lastNewEntryDismiss ? dayjs(profile?.lastNewEntryDismiss) : startDate
+            const newListingIds = watchlistIds.filter(id => {
+                const newListingDate = getEntryFromId(id)?.newListingsDate ? dayjs(getEntryFromId(id).newListingsDate) : null
+                return (newListingDate && newListingDate.isAfter(lastMessageDate))
+            })
+            if (newListingIds?.length > 0) {
+                return <NewListingsMessage entryCount={newListingIds?.length} handleDismiss={handleDismiss}/>
+            }
+        }
+    }, [filters?.collection, getEntryFromId, handleDismiss, profile, watchlistIds])
 
     const value = useMemo(() => ({
-        displayMessage,
-        setDisplayMessage,
         systemMessage,
         handleDismiss
     }), [
-        displayMessage,
-        setDisplayMessage,
         systemMessage,
         handleDismiss
     ])
